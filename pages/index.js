8c0d60ade93bd43206d694d38bc81b1d69c916b5
strzelus11 <103840690+strzelus11@users.noreply.github.com>
2024-07-10 20:03:59 +0200

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Featured from "@/components/Featured";
+import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-const inter = Inter({ subsets: ["latin"] });
+export default function HomePage() {
+	return (
+		<div className="flex flex-col">
+			<div className="h-screen flex flex-col">
+                <Header />
+                <Featured />
+            </div>
+		</div>
+	);
+}
 
-export default function Home() {
-  return (
-    <div></div>
-  );
+export async function getServerSideProps() {
+    const featuredProductId = "hieahudfhwqeidhh"
+    await mongooseConnect();
+    const featuredProduct = await Product.findById(featuredProductId);
+    return {
+        props: {
+            featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+
+        }
+    }
 }
